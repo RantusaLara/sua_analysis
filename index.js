a50207c1e6ec9cf81ccfffd299edd5a0cb8b35cf
Jana Janković <jana.jankovic@student.um.si>
2023-01-29 11:47:25 +0100

    endpoints

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const Inert = require("@hapi/inert");
 const Vision = require("@hapi/vision");
 const HapiSwagger = require("hapi-swagger");
 const Pack = require("./package");
+const Mongoose = require("mongoose");
+require("dotenv/config");
+const Joi = require("joi");
 
 (async () => {
   const server = Hapi.server({
@@ -10,9 +13,10 @@ const Pack = require("./package");
     host: "localhost",
   });
 
+  /**SWAGGER---------------------------------------------- */
   const swaggerOptions = {
     info: {
-      title: "Test API Documentation",
+      title: "API Documentation Services Analysis",
       version: Pack.version,
     },
   };
@@ -25,21 +29,137 @@ const Pack = require("./package");
       options: swaggerOptions,
     },
   ]);
+  /**SWAGGER---------------------------------------------- */
 
+  /**PODATKOVNA BAZA-------------------------------------- */
+  Mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log("Connected to databse %s", process.env.MONGO_URI);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  const AnalysisSchema = Mongoose.Schema({
+    url: {
+      type: String,
+      required: true,
+    },
+    last_called: {
+      type: Date,
+      required: true,
+    },
+    calls: {
+      type: Number,
+      required: true,
+    },
+  });
+
+  const AnalysisModel = Mongoose.model("Analysis", AnalysisSchema);
+  /**PODATKOVNA BAZA-------------------------------------- */
+
+  /**STREŽNIK---------------------------------------------- */
   try {
     await server.start();
     console.log("Server running on %s", server.info.uri);
   } catch (err) {
     console.log(err);
   }
+  /**STREŽNIK----------------------------------------------- */
 
-  server.route({
-    method: "POST",
-    path: "/",
-    handler: (request, h) => {
-      return h
-        .response({ message: "Successfully sent message to queue" })
-        .code(201);
+  /**KONČNE TOČKE------------------------------------------- */
+  const routes = [
+    {
+      method: "GET",
+      path: "/",
+      options: {
+        tags: ["api"], //SWAGGER
+      },
+      handler: (request, h) => {
+        try {
+          const analysis = AnalysisModel.find();
+          return h.response(analysis).code(201);
+        } catch (error) {
+          return h
+            .response({ content: "Server error." + error, error: true })
+            .code(500);
+        }
+      },
     },
-  });
+    {
+      method: "GET",
+      path: "/last",
+      options: {
+        tags: ["api"], //SWAGGER
+      },
+      handler: (request, h) => {
+        try {
+          let analysis = AnalysisModel.find();
+          analysis = analysis.sort({ calls: -1 });
+          return h.response(analysis[0]).code(201);
+        } catch (error) {
+          return h
+            .response({ content: "Server error." + error, error: true })
+            .code(500);
+        }
+      },
+    },
+    {
+      method: "GET",
+      path: "/latest",
+      options: {
+        tags: ["api"], //SWAGGER
+      },
+      handler: (request, h) => {
+        try {
+          let analysis = AnalysisModel.find();
+          analysis = analysis.sort({ last_called: -1 });
+          return h.response(analysis[0]).code(201);
+        } catch (error) {
+          return h
+            .response({ content: "Server error." + error, error: true })
+            .code(500);
+        }
+      },
+    },
+    {
+      method: "POST",
+      path: "/",
+      options: {
+        tags: ["api"], //SWAGGER
+        validate: {
+          payload: Joi.object({
+            url: Joi.string().required(),
+          }),
+        },
+      },
+      handler: (request, h) => {
+        try {
+          const url = request.body.url;
+          const data = AnalysisModel.findOne({ url: url });
+          if (data == undefined)
+            return h
+              .response({ message: "Url not found", error: true })
+              .code(404);
+          AnalysisModel.updateOne(
+            { url: url },
+            {
+              $set: {
+                calls: data.calls++,
+                last_called: new Date(),
+              },
+            }
+          );
+          return h.response({ message: "Updated", error: false }).code(201);
+        } catch (error) {
+          return h
+            .response({ content: "Server error." + error, error: true })
+            .code(500);
+        }
+      },
+    },
+  ];
+
+  server.route(routes);
+  /**KONČNE TOČKE------------------------------------------- */
 })();
