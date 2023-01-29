76ae62e200ac49925a8d024096153767542997a9
Jana Janković <jana.jankovic@student.um.si>
2023-01-29 12:54:56 +0100

    swagger fix

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const Joi = require("joi");
 (async () => {
   const server = Hapi.server({
     port: 3000,
-    host: "0.0.0.0",
   });
 
   /**SWAGGER---------------------------------------------- */
